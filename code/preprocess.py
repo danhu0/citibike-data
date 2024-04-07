5242806cd6be1f3c58cea76ad62df461e6656b02
Daniel Hu <daniel_hu4@brown.edu>
2024-04-06 20:56:53 -0400

    feat: added in loading file by month and year

--- code/preprocess.py
+++ code/preprocess.py
@@ -5,20 +5,16 @@ from datetime import datetime
 
 # ANSI escape codes for text colors
 class colors:
-    RED = '\033[91m'
     GREEN = '\033[92m'
-    CYAN = '\033[96m'
-    BLUE = '\033[94m'
     END = '\033[0m' 
 
 def download_file(url, directory, filename):
-    # Concatenate directory path and filename
     filepath = os.path.join(directory, filename)
     try:
         urllib.request.urlretrieve(url, filepath)
         print(f"{colors.GREEN}File downloaded successfully!{colors.END}")
     except urllib.error.URLError as e:
-        print(f"{colors.RED}Failed to download file: {e.reason}{colors.END}")
+        print(f"Failed to download file: {e.reason}")
         return
 
 def unzip_file(zip_filename, extract_dir):
@@ -27,21 +23,31 @@ def unzip_file(zip_filename, extract_dir):
     print(f"{colors.GREEN}File extracted successfully!{colors.END}")
 
 def main():
-    url = input(f"{colors.CYAN}Enter the URL of the ZIP file to download: {colors.END}")
+    while True:
+        mode = input("Manual URL or Date Input (manual/date): ")
+        if mode == "manual" or mode == "date":
+            break
+        else:
+            print("usage: Enter 'manual' or 'date'")
+
+    if mode == "manual":
+        url = input("Enter the URL of the ZIP file to download: ")
+    elif mode == "date":
+        year = input("Year: ")
+        month = int(input("Month (1-12, or 0 if importing a whole year): "))
+        url = f"https://s3.amazonaws.com/tripdata/JC-{year}{month:02d}-citibike-tripdata.csv.zip"
+    
     zip_filename = 'zip_archives_' + datetime.now().strftime("%Y-%m-%d %H:%M:%S") +'.zip'
     if not os.path.exists("zip_archives"):
         os.makedirs("zip_archives")
     download_directory = os.path.join(os.getcwd(), "zip_archives")
 
-
-    # Download the file to the specified directory
     download_file(url, download_directory, zip_filename)
 
-    extract_dir = input(f"{colors.CYAN}Enter the directory to extract the contents to: {colors.END}")
+    extract_dir = input("Enter the directory to extract the contents to: ")
     if not os.path.exists(extract_dir):
         os.makedirs(extract_dir)
 
-    # Unzip the file into the specified extraction directory
     unzip_file(os.path.join(download_directory, zip_filename), extract_dir)
 
 if __name__ == "__main__":
